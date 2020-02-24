71b3bfd4e554e142e0a268e3ae7bc02f96519ba4
hiroshichikamori <57493590+hiroshichikamori@users.noreply.github.com>
2020-02-24 18:14:16 +0900

    js応用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
     end
   end
 
+  # def index
+  #   @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"]).where.not(id: params[:user_ids]).limit(10)
+  #   respond_to do |format|
+  #     format.html
+  #     format.json
+  #   end
+  # end
+
   def edit
   end
 
